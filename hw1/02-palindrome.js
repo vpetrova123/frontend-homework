350e74e9fb697556a54eb3f24af7e6e974a6848a
Viktoriya <petrovav971@gmail.com>
2023-04-07 18:19:07 -0700

    Completed hw1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,36 @@
-const elem = document.querySelector('input');
+const section = document.querySelector("section");
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+//Input event listener
+elem.addEventListener("input", handleInput);
+
+//Create a new div for the output
+const outputDiv = document.createElement("div");
+outputDiv.setAttribute("id", "output");
+section.append(outputDiv);
+
+//Handle the event listener
+function handleInput(event) {
+  event.preventDefault();
+  const input = event.target.value;
+  let reverseInput = input.split("").reverse().join("");
+  console.log(input);
+
+  if (input < 0) {
+    outputDiv.style.color = "red";
+    outputDiv.textContent = "Please enter a positive number.";
+    return;
+  }
+  if (input === "") {
+    outputDiv.style.color = "red";
+    outputDiv.textContent = "Please enter a number.";
+    return;
+  }
+  if (input === reverseInput) {
+    outputDiv.style.color = "green";
+    outputDiv.textContent = "Yes. This is a palindrome!";
+  } else {
+    outputDiv.style.color = "red";
+    outputDiv.textContent = "No. Try again.";
+  }
+}
